fa0f6002d65b9e595df42f3a466ea74f3c0f8d3d
AmanWorku <amexworku2014@gmail.com>
2022-12-21 14:02:18 +0300

    Create the game and fetch ID of the game from API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './style.css';
 import './modules/listeners.js';
+
+const newGame = async () => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'Spiderman',
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+newGame();
